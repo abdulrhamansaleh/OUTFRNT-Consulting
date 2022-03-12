cad7855a7cfb87f27217250157a0192dcef9eda4
Abdul <pureexec@gmail.com>
2022-03-11 21:29:26 -0500

    finalized site

--- outfrnt/manage.py
+++ outfrnt/manage.py
@@ -1,9 +1,7 @@
 import os
 import sys
-import dotenv 
 
 def main():
-    dotenv.read_dotenv()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'outfrnt.settings')
     try:
         from django.core.management import execute_from_command_line
